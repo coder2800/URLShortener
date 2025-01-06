fa9a12a8d07dc6f8da9788d9579dff80468018a3
coder2800 <devanshagarwal100@gmail.com>
2025-01-06 22:44:20 +0530

    Added profile photo functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,66 @@
 const User = require("../models/user");
 const { setUser } = require("../service/auth");
 
+
 //signup controller
 const createUser = async (req, res) => {
-    const body = req.body;
-    if (!body.email || !body.password || !body.name) {
+    try {
+        const body = req.body;
+        console.log(body);
+        if (!body.email || !body.password || !body.name) {
+            return res.render("error", {
+                errorMessage: "Name or Email or password is not present"
+            })
+        }
+        const email = body.email;
+        const password = body.password;
+        const name = body.name;
+        const role = body.role || "normal";
+        const user = await User.create({
+            Name: name,
+            Email: email,
+            Password: password,
+            Role: role
+        })
+        const token = setUser(user);
+        res.cookie('uid', token);
+        return res.render("success", {
+            message: "Wooho! User created successfully!"
+        });
+    }
+    catch(error){
         return res.render("error", {
-            errorMessage: "Name or Email or password is not present"
+            errorMessage: error.message
         })
     }
-    const email = body.email;
-    const password = body.password;
-    const name = body.name;
-    const role = body.role || "normal";
-    const user = await User.create({
-        Name: name,
-        Email: email,
-        Password: password,
-        Role: role
-    })
-    const token = setUser(user);
-    res.cookie('uid', token);
-    return res.render("success", {
-        message: "Wooho! User created successfully!"
-    });
 }
 
 //login user controller
 const loginUser = async (req, res) => {
-    const {email, password} = req.body;
-    if (!email || !password) {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.render("error", {
+                errorMessage: "Oops! Email or Password is not present"
+            })
+        }
+        const user = await User.findOne({ Email: email, Password: password });
+        if (!user) {
+            return res.render("error", {
+                errorMessage: "Oops! Email or password is not correct"
+            })
+        }
+        const token = setUser(user);
+        res.cookie('uid', token);
+        return res.render("success", {
+            message: "Wooho! Login successful!"
+        });
+    }
+    catch(error){
         return res.render("error", {
-            message: "Oops! Email or Password is not present"
+            errorMessage: error.message
         })
     }
-    const user = await User.findOne({ Email: email, Password: password });
-    if (!user) {
-        return res.render("error", {
-            message: "Oops! Email or password is not correct"
-        })
-    }
-    const token = setUser(user);
-    res.cookie('uid', token);
-    return res.render("success", {
-        message: "Wooho! Login successful!"
-    });
 }
 
 module.exports = {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const router = express.Router();
-const {createUser, loginUser} = require("../controllers/userController")
+const { createUser, loginUser } = require("../controllers/userController")
+const multer = require('multer');
 
-router.post("/", createUser);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      return cb(null, './uploads/'); // Directory to save files
+    },
+    filename: (req, file, cb) => {
+      return cb(null, `${Date.now()}-${file.originalname}`); // Unique file name
+    },
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single('profilePhoto'), createUser);
 
 router.post("/login", loginUser);
 
